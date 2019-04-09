e212f86d56c5356f6888f3f3827a2b4d520a7f77
moonstar-x <christian.lopez99@outlook.com>
2019-04-09 15:11:12 -0500

    Refactored Sheets fetch
    Fetch code is now reusable and each pack can easily be downloaded.

---
+++ src/modules/classes/Pack.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const GoogleSpreadsheets = require('google-spreadsheets');
+
+class Pack {
+  constructor(spreadsheetID, packID, packName, range) {
+    this.absolutePath = './src/data/packs/' + packID + '.json';
+    this.relativePath = '../../data/packs/' + packID + '.json';
+
+    this.pack = this._loadPackJSON();
+    this.packName = packName;
+    this.packID = packID;
+    this._initializeObject();
+
+    this.spreadsheetID = spreadsheetID;
+
+    this.worksheet = range[0];
+    this.whiteRange = range[1];
+    this.blackRange = range[2];
+  }
+
+  fetchData() {
+    GoogleSpreadsheets({
+      key: this.spreadsheetID,
+      auth: null
+    }, (err, spreadsheet) => {
+      if (err) console.error(err);
+      spreadsheet.worksheets[this.worksheet].cells({
+        range: this.whiteRange
+      }, (err, cells) => {
+        if (err) console.error(err);
+        for(const row in cells.cells) {
+          for (const col in cells.cells[row]) {
+            this.pack.white.push(cells.cells[row][col].value);
+          }
+        }
+
+        spreadsheet.worksheets[this.worksheet].cells({
+          range: this.blackRange
+        }, (err, cells) => {
+          if (err) console.error(err);
+          for (const row in cells.cells) {
+            const obj = cells.cells[row];
+            const special = obj[Object.keys(obj)[1]] ? obj[Object.keys(obj)[1]].value.split(' ') : [];
+            
+            const pickIndex = special.indexOf('PICK');
+            const drawIndex = special.indexOf('DRAW');
+            const pick = pickIndex > -1 ? parseInt(special[pickIndex + 1]) : 1;
+            const draw = drawIndex > -1 ? parseInt(special[drawIndex + 1]) : 1;
+  
+            this.pack.black.push({
+              'content': obj[Object.keys(obj)[0]].value,
+              'pick': pick,
+              'draw' : draw
+            });
+          }
+
+          this._setQuantity();
+          this._writeData();
+        });
+      });
+    });
+  }
+
+  _loadPackJSON() {
+    if (fs.existsSync(this.absolutePath)) {
+      return require(this.relativePath);
+    } else {
+      fs.writeFileSync(this.absolutePath, JSON.stringify({}, null, 2), function(err) {
+        if (err) return console.error(err);
+      });
+      return require(this.relativePath);
+    }
+  }
+
+  _setQuantity() {
+    this.pack.quantity.black = this.pack.black.length
+    this.pack.quantity.white = this.pack.white.length
+    this.pack.quantity.total = this.pack.quantity.black + this.pack.quantity.white
+  }
+
+  _initializeObject() {
+    this.pack.pack = {
+      'name': this.packName,
+      'id': this.packID
+    }
+    this.pack.black = []
+    this.pack.white = []
+    this.pack.quantity = {
+      'black': 0,
+      'white': 0,
+      'total': 0
+    }
+  }
+
+  _writeData() {
+    fs.writeFileSync(this.absolutePath, JSON.stringify(this.pack, null, 2), function(err) {
+      if (err) return console.log(err); 
+    });
+  }
+}
+
+module.exports = {
+  Pack: Pack
+}
--- src/modules/google-sheets-fetch.js
+++ src/modules/google-sheets-fetch.js
@@ -1,12 +1,36 @@
-const fs = require('fs');
+const { Pack } = require('./classes/Pack.js');
 
 // ID of the Google Sheets with all the data
 const spreadsheetID = '1NeIMmSMT3nYv_YzucrToYouA4SMWTWr8G18lfCE61ZM';
 
-// Run Every Packs Fetch Script
-const packFiles = fs.readdirSync(__dirname + '/packs').filter(file => file.endsWith('.js'));
+// Google Sheets Ranges
+// Object(Key: Pack ID, Value: [WORKSHEET_INDEX, WHITE, BLACK])
+const RANGES = {
+  'main_deck': [4, 'B293:B1547', 'B6:C287'],
+  'canadian_conversion_kit': [4, 'B1571:B1592', 'B1563:C1569'],
+  'uk_conversion_kit': [4, 'B1619:B1692', 'B1598:C1617']
+}
 
-for (const file of packFiles) {
-  const fetchScript = require('./packs/' + file);
-  fetchScript.execute(spreadsheetID);
+// Pack Names
+// Object(Key: Pack ID, Value: Pack Name)
+const PACKNAMES = {
+  'main_deck': 'Main Deck',
+  'canadian_conversion_kit': 'Canadian Conversion Kit',
+  'uk_conversion_kit': 'UK Conversion Kit',
+}
+
+// Validate RANGES and PACKNAMES objects.
+if (Object.keys(RANGES).length != Object.keys(PACKNAMES).length){
+  throw new Error('RANGES and PACKNAMES do not correspond.');
+}
+for (const key in RANGES) {
+  if (!PACKNAMES.hasOwnProperty(key)) {
+    throw new Error('RANGES and PACKNAMES do not correspond.');
+  }
+}
+
+// Fetch all packs
+for (const key in PACKNAMES) {
+  const pack = new Pack(spreadsheetID, key, PACKNAMES[key], RANGES[key]);
+  pack.fetchData();
 }
